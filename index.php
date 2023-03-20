b6471d805aa1b6b38fa54d937559dc44e2001130
masayoshiyuto <y-masayoshi@kazehikaru.co.jp>
2023-03-20 19:22:41 +0900

    コミット名

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php echo "Hello PHP";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP掲示板</title>
+    <link rel = "stylesheet" herf = "style.css">
+</head>
+<body>
+
+</body>
+</html>
